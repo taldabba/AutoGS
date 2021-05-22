24db738dab84155b8c0f16a271e037efc9bc44c4
adamikram <adamikram5@gmail.com>
2021-05-22 00:58:25 -0400

    Update chatSpam.py

--- desktop/chatSpam.py
+++ desktop/chatSpam.py
@@ -1,42 +1,32 @@
-from pyautogui import press, typewrite, hotkey, position, moveTo,click,moveRel
+from pyautogui import press, typewrite, hotkey, position, moveTo,click
 
 import time
-import random
+
 def spamPicture():
 	# pyperclip.copy(link)
 	hotkey('ctrl','v')
 	press('enter')
 
-for x in range(5):
+for x in range(3):
 	print(f'SPAMMING IN: {x} SECONDS')
-	# time.sleep(1)
+	time.sleep(1)
 
-# for x in range(100):
 while True:
 	print(position())
-	xRange = 1919
-	yRange = 1079
-
-	xCoord = random.randint(0,xRange)
-	yCoord = random.randint(0,yRange)
-	moveTo(xCoord,yCoord)
-	
-	# spamPicture()
-	# click()
-	# moveRel()
-
-	# time.sleep(0.1)
+	for x in range(1000):
+		moveTo(630,700)		
+		spamPicture()
+		click()
 
 
 
 
 
-# for x in range(3):
-# 	spamPictu
-# 	time.sleep(0.2)
+		time.sleep(2)
 
 
-	# if x == 1:
+#🤣sussy imposter XD 🤣
+# 🤣sussy imposter XD 🤣
+#XD
 
-#630 700
-#
+XDXD
