a97fd75452ca628f0e7bb2e38dd75422bd58dfa3
taldabba <taldabba@gmail.com>
2021-05-24 16:55:51 -0400

    website on flask program

--- webapp/main.py
+++ webapp/main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("templates/interface.html")
+    return render_template("interface.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
