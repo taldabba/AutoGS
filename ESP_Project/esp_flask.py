dd89056a4ab760a6fb17f5b64593c2681b485dca
adamikram <adamikram5@gmail.com>
2021-05-27 01:39:20 -0400

    first working sensors with esp and flask!!!

--- ESP_Project/esp_flask.py
+++ ESP_Project/esp_flask.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask,request
  
 app = Flask(__name__)
  
 @app.route('/helloesp')
 def helloHandler():
-    return 'Hello ESP8266, from Flask'
+	temperature = request.args.get("temperature")
+	humidity = request.args.get("humidity")
+	soilMoisturePercent = request.args.get("soilMoisturePercent")
+	return f'Hello ESP8266, from Flask -- temperature={temperature}, humidity={humidity}, soilMoisturePercent={soilMoisturePercent} '
  
-app.run(host='0.0.0.0', port= 8090)
+app.run(host='0.0.0.0', port= 8090,debug=True)
--- ESP_Project/httpsCode_Arduino/httpsCode_Arduino.ino
+++ ESP_Project/httpsCode_Arduino/httpsCode_Arduino.ino
@@ -1,6 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <SPI.h>
 
 #include "DHTesp.h"
 #ifdef ESP32
@@ -8,44 +11,16 @@
 #error Select ESP8266 board.
 #endif
 
-#include <Wire.h>
-#include <SPI.h>
-
 DHTesp dht;
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-String getSensors(){
-
-  //capcitive soil moisture variables
-  const int AirValue = 654;   
-  const int WaterValue = 305; 
-  const int SensorPin = A0;
-  int soilMoistureValue = 0;
-  int soilMoisturePercent=0;
-  
-  float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
-
-  soilMoistureValue = analogRead(SensorPin);  //put Sensor insert into soil
-  soilMoisturePercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-
-  String output = (String(humidity) + "\t"+ String(temperature) + "\t" +String(soilMoisturePercent));
-  return output;  
-}
-
-
-
-
-
-
-
-
- 
 void setup () {
  
   Serial.begin(115200);
- 
-  dht.setup(5, DHTesp::DHT22); //dht setup
+   
+  dht.setup(16, DHTesp::DHT22); //dht setup
 
+  lcd.begin(16,2);//lcd setup
   //wifi setup
   WiFi.begin("unit2803", "platinum2803n2"); 
   while (WiFi.status() != WL_CONNECTED) { 
@@ -53,18 +28,26 @@ void setup () {
     Serial.println("Connecting.."); 
   }
 
-  
   Serial.println("Connected to WiFi Network"); 
 }
- 
-void loop() {
-  Serial.println(getSensors());
+
+void displayLCD(int temperature, int humidity, int soilMoisturePercent) {
+  String sensorStringLn1 = "TMP:" + String(temperature) + "C  HUM:" + String(humidity) +"%";
+  String sensorStringLn2 = "SL:" + String(soilMoisturePercent) + "%";
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(sensorStringLn1);
+  lcd.setCursor(0,2);
+  lcd.print(sensorStringLn2);  
+}
+
+void sendToWebserver(int temperature, int humidity, int soilMoisturePercent) {
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
  
     HTTPClient http;  //Declare an object of class HTTPClient
     WiFiClient wifi;
-    http.begin(wifi,"http://192.168.0.123:8090/helloesp"); //Specify request destination
- 
+    http.begin(wifi,"http://192.168.0.123:8090/helloesp?temperature=" + String(temperature) + "&humidity=" + String(humidity) + "&soilMoisturePercent=" + String(soilMoisturePercent)); //Specify request destination
+
     int httpCode = http.GET(); //Send the request
  
     if (httpCode > 0) { //Check the returning code
@@ -77,6 +60,22 @@ void loop() {
     http.end();   //Close connection
  
   }
- 
+}
+
+void loop() {
+  const int AirValue = 654;   
+  const int WaterValue = 305; 
+  const int SensorPin = A0;
+  int soilMoistureValue = 0;
+  int soilMoisturePercent=0;
+  
+  int humidity = dht.getHumidity();
+  int temperature = dht.getTemperature();
+  soilMoistureValue = analogRead(SensorPin);  //put Sensor insert into soil
+  soilMoisturePercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+  
+  displayLCD(temperature,humidity, soilMoisturePercent );
+  sendToWebserver(temperature,humidity, soilMoisturePercent );
+  
   delay(2000);    //Send a request every 10 seconds
 } 
