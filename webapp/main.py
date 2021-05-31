702891e7d7274c7077c313403978e85c9bcde1d4
Taim <taldabba@gmail.com>
2021-05-31 00:19:11 -0400

    post request

--- webapp/main.py
+++ webapp/main.py
@@ -1,10 +1,29 @@
 from flask import Flask, redirect, url_for, render_template, request
+import logging
 
 app = Flask(__name__)
 
-@app.route("/")
+def getPost(app, name):
+    try:
+        user = request.form[name]
+        return True
+    except:
+        return False
+
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("interface.html")
+    if request.method == "POST":
+        getPost(app, "led-button")
+        if (getPost(app, "led-button")):
+            app.logger.info("bruhLEDChungus")
+        elif (getPost(app, "sendRGB")):
+            app.logger.info("rgbChungus")
+        elif (getPost(app, "waterSend")):
+            app.logger.info("waterChungus")
+
+        return render_template("interface.html")
+    else:
+        return render_template("interface.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
